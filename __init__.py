25a92c29dae02244d91546611b03cfa06a0c7069
mikeold-2008 <117850111+mikeold-2008@users.noreply.github.com>
2022-11-09 21:08:50 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,16 +6,10 @@ import requests
 class CarbonIntensityUk(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        
-    def fetch_last_half_hour()-> str:
-        BASE_URL = "https://api.carbonintensity.org.uk/intensity"
-        intensity = requests.get(BASE_URL).json()['data'][0]
-        return intensity["intensity"]["index"]
 
     @intent_file_handler('uk.intensity.carbon.intent')
     def handle_uk_intensity_carbon(self, message):
-        intensity = ''
-        intensity = self.fetch_last_half_hour()
+        intensity = 'house'
         
         self.speak_dialog('uk.intensity.carbon', data={
             'intensity': intensity
