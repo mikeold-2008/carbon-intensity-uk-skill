a1a0b2ee79e1bf049730796e554f053ac9a39dd6
mikeold-2008 <117850111+mikeold-2008@users.noreply.github.com>
2022-11-09 21:21:05 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ class CarbonIntensityUk(MycroftSkill):
 
     @intent_file_handler('uk.intensity.carbon.intent')
     def handle_uk_intensity_carbon(self, message):
+        requests.get(https://api.carbonintensity.org.uk/intensity).json()['data]['0']
         intensity = "house tall"
         
         self.speak_dialog('uk.intensity.carbon', data={
